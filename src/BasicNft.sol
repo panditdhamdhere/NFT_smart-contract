a8cb9ebfeb49f802e869ceb040ad3970dfdf5c2b
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-03 10:03:44 +0530

    nft contract added

---
+++ src/BasicNft.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+}
