63abfcfab4fb5d7b73673aa39e7176c5a6ae8a3b
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-04 16:03:46 +0530

    another tested and passed

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -3,13 +3,16 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
-// import {DeployBasicNft} from "../src/script/DeployBasicNft.s.sol";
+
 import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function setUp() public {
         deployer = new DeployBasicNft();
@@ -23,6 +26,17 @@ contract BasicNftTest is Test {
         assert(
             keccak256(abi.encodePacked(expectedName)) ==
                 keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(PUG)) ==
+                keccak256(abi.encodePacked(basicNft.tokenURI(0)))
         );
     }
 }
