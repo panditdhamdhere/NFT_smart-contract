bd40f51fb271f33f14c8ce83b4e3af54b0d13b57
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-05 23:34:39 +0530

    test failed

---
+++ test/DeployMoodNftTest.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public deployer;
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+    }
+
+    function testConvertSvgToUri() public view {
+        string memory expectedUri = "klsrfvsilklfsdfolisdflkvs";
+        string memory svg = "kldjdssvkdvsk";
+        string memory actualUri = deployer.svgToImageURI(svg);
+
+        assert(
+            keccak256(abi.encodePacked(actualUri)) ==
+                keccak256(abi.encodePacked(expectedUri))
+        );
+    }
+}
