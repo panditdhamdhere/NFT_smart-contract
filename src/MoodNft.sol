265fbe419b6245a59a8afbb84c7079d2fdb7fe87
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-05 11:27:10 +0530

    fixed prlm

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,20 +2,53 @@
 
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
-contract MoodNftis ERC721 {
+contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
-    string private s_sadSvg;
-    string private s_happySvg;
-constructor(string memory sadSvg, string memory happySvg) ERC721("Mood NFT", "MN") {
-s_tokenCounter = 0;
-s_sadSvg = sadSvg;
-s_happySvg = happySvg;
-}
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
 
-function mintNft() public {
-    _safeMint(msg.sender, s_tokenCounter);
-    s_tokenCounter++;
-}
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
+    ) ERC721("Mood NFT", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
+        s_tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageURI;
+
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageURI = s_happySvgImageUri;
+        } else {
+            imageURI = s_sadSvgImageUri;
+        }
+
+        string memory tokenMetadata = string.concat(
+            '{"name": "',
+            name(),
+            '", "description": "An NFT that reflects the owner\'s mood.", "attributes": [ { "trait_type": "moodiness", "value": "100" } ], "image": "',
+            imageURI,
+            '"}'
+        );
+    }
 }
